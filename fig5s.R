f9601b7f84f6fa97146231dbb1564141a453f9af
lilab2022 <112673632+lilab2022@users.noreply.github.com>
2023-08-09 18:23:24 +0200

    Update fig5s.R

--- fig5s.R
+++ fig5s.R
@@ -2,116 +2,11 @@
 #### writer： Feng Ruoqing
 #### Date：2022-10-4
 
-###Fig5s sup
-mc_sc3 = mac12.metatable
-mc_sc3$tissue = mc_sc3$subtype
-mc_sc3$major_type = "macrophage"
-n <- 100
-ntime <- 150
-
-pop <- 'macrophage'
-tissueA <- "pre_PraM"; cutA <- 30
-tissueB <- "PraM"; cutB <- 30
-
-get_n_cells_per_pop_per_disease(mc_sc3, n, pop, tissueA, cutA, diag =F)
-get_n_cells_per_pop_per_disease(mc_sc3, n, pop, tissueB, cutB, diag =F)
-
-ind_diff <- diff_calc(mc_sc3, a_reduced,pop, n, ntime, tissueA, tissueB, cutA, cutB)
-ind_diff_stat <- diff_stat(ind_diff, ntime)
-
-f_cut <- 1.5
-q_cut <- 0.05
-pct_cut <- 0.05
-ind_diff_stat_good <- ind_diff_stat[ind_diff_stat$pctA.mean > pct_cut | ind_diff_stat$pctB.mean > pct_cut,]
-f <- log2((ind_diff_stat_good$avgB.mean + 0.01)/(ind_diff_stat_good$avgA.mean + 0.01))
-q <- log(-(log(ind_diff_stat_good$wilcox.p.mean, 10)) + 1)
-dat <- data.frame(g = rownames(ind_diff_stat_good), f = f, q = q)
-#dat$f = -dat$f
-up <- dat[dat$f > log2(f_cut) & dat$q  > log(-log10(q_cut) + 1),]
-message('up: ', nrow(up))
-dn <- dat[dat$f < -log2(f_cut) & dat$q  > log(-log10(q_cut) + 1),]
-message('dn: ', nrow(dn))
-rownames(dat) <- rownames(ind_diff_stat_good)
-diff_genes <- rownames(dat)[abs(dat$f) > log2(f_cut) & dat$q > log(-log10(q_cut) + 1)]
-message('diff: ', length(diff_genes))
-dat$sig <- ""
-dat[diff_genes,]$sig <- diff_genes
-dat <- dat[order(dat$sig),]
-dat$group = "non"
-dat$group[dat$f > log2(f_cut) & dat$q > log(-log10(q_cut) + 1)] = "B"
-dat$group[dat$f < -log2(f_cut) & dat$q > log(-log10(q_cut) + 1)] = "A"
-
-deg = unique(dat$sig)
-deg = c("C5AR1","CXCL3","CXCL8","DAB2","PTGS2","SOD2","TNF","IL1B","HMGA2","CD36","CD83","IL1A",
-        "VEGFA","GLUL","ICAM1")
-
-dat$sig = ""
-for(i in deg){
-  dat$sig[dat$g == i] = i
-}
-
-p1 = ggplot(dat, aes(f, q, label = sig,fill = group)) +
-  geom_point( size=3,shape= 21,color = "grey66")+
-  scale_fill_manual(values=c('#2CA02CFF','#FF7F0EFF', "grey88"))+
-  geom_text_repel(data = dat[dat$sig != "",],max.overlaps = 100,size = 4) +
-  geom_vline(xintercept = log2(f_cut) , linetype="dashed", color="grey66") + 
-  geom_vline(xintercept = -log2(f_cut), linetype="dashed", color="grey66") + 
-  ylab('log((-log10 q) + 1)') + xlab('log2 f')+
-  annotate(geom="text", x=-2, y=3, label="pre_PraM",  fontface="bold",colour='#2CA02CFF', size=5)+
-  annotate(geom="text", x=2, y=3, label="PraM",  fontface="bold",colour='#FF7F0EFF', size=5)+
-  theme_bw()+theme_classic()+xlab("log2(fold change)")+ylab("q value")+
-  theme(legend.position = "none",
-        axis.text = element_text(size=14))
-ggsave("E:\\fig5/deg_volcano_newnormalize.pdf",p1,width = 5,height = 5,dpi = 300)
 
 
-###Fig5s B
-figre5_mac_ratio = function(the_metatable,the_path,thew , theh){
-  the_metatable$new = paste0(the_metatable$tissue,the_metatable$subtype)
-  for (new in unique(the_metatable$new)) {
-    if (nrow(the_metatable[the_metatable$new == new,])<10){
-      the_metatable= the_metatable[-which(the_metatable$new == new),]
-    }
-  }
-  
-  the_metatable = the_metatable[,c("time","subtype","Well_ID","tissue")]
-  the_metatable = the_metatable[-which(the_metatable$time == "Adult"),]
-  the_metatable = the_metatable[-which(the_metatable$time == "week19"),]
-  the_metatable$time <- factor(the_metatable$time,levels=time_order,ordered = TRUE)
-  
-  tmp2 = the_metatable[,c("time","subtype")]
-  
-  tmp2 = tmp2[!duplicated(tmp2),]
-  rownames(tmp2) = NULL
-  tmp2$rate = 0
-  for(t in rownames(tmp2)){
-    tmp2[t,"rate"] = nrow(the_metatable[the_metatable$time == tmp2[t,"time"] & the_metatable$subtype == tmp2[t,"subtype"],])/nrow(the_metatable[the_metatable$time == tmp2[t,"time"],])
-  }
-  
-  the_p1 = ggplot(tmp2,aes(x = time, 
-                           stratum = subtype, 
-                           alluvium = subtype,
-                           y = rate,
-                           fill = subtype,
-                           label = subtype)) +
-    scale_y_continuous(label = scales::percent_format(),
-                       expand=c(0,0)) +
-    scale_fill_manual(values = as.character(mac.color),breaks = names(mac.color)) +
-    geom_flow() +
-    geom_stratum(width = 0.6,colour = "grey30") +
-    theme(axis.title=element_blank(),
-          axis.text.x.bottom = element_text(angle = 45,hjust = 1,vjust = 1),
-          legend.title=element_blank(),
-          panel.background=element_blank(),
-          axis.line=element_line(),
-          axis.ticks=element_line(),
-          axis.text = element_text(size = 12),
-          legend.position = "none")
-  ggsave(the_path,the_p1,width =thew,height =theh,dpi = 300)
-}
-figre5_mac_ratio(the_metatable = mac12.metatable,the_path = "E:\\fig5/mac12_dynamic.pdf",thew = 8,theh = 5)
 
-###Fig5s D
+
+###Fig5s C
 
 mac1.metatable = mac12.metatable[mac12.metatable$subtype == "PraM",]
 mac1.metatable = mac1.metatable[mac1.metatable$time != "Adult",]
@@ -201,7 +96,7 @@ p1 = ggplot(final_marker)+
   theme(legend.position = 'none',axis.text.x = element_text(angle = 45,hjust = 1,vjust = 1))
 ggsave("E:\\fig5/deg.pdf",p1,width = 8,height = 6,dpi = 300)
 
-###Fig5s E
+###Fig5s D
 mac1.metatable = mac12.metatable[mac12.metatable$time != "Adult",]
 mac1.metatable = mac1.metatable[,colnames(metatable)]
 mac1.metatable = mac1.metatable[mac1.metatable$tissue %in% c("Kidney","Lung","Adrenalgland",
